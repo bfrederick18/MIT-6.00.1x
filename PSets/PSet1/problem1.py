ff0e81165e5c7e486edf328ff9f890ed70bd43ce
Brandon Frederick <hitek1218@outlook.com>
2022-09-03 12:36:35 -0700

    Add PSets, PSet1, and problem1.py and problem2.py

---
+++ PSets/PSet1/problem1.py
@@ -0,0 +1,19 @@
+"""
+Assume s is a string of lower case characters.
+
+Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl', your program should print:
+
+Number of vowels: 5
+"""
+
+s = 'azcbobobegghakl'  # Test String
+
+# Paste your code into this box
+v = ['a', 'e', 'i', 'o', 'u']  # Vowels
+count = 0
+
+for char in s:
+    if char in v:
+        count += 1
+
+print('Number of vowels: ' + str(count))
---
+++ PSets/PSet1/problem2.py
@@ -0,0 +1,18 @@
+"""
+Assume s is a string of lower case characters.
+
+Write a program that prints the number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl', then your program should print
+
+Number of times bob occurs is: 2
+"""
+s = 'b'
+
+# Paste your code into this box 
+bob = 'bob'
+count = 0
+
+for i in range(len(s) - len(bob) + 1):
+    if s[i:i + len(bob)] == bob:
+        count += 1
+
+print('Number of times bob occurs is: ' + str(count))
